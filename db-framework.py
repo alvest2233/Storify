bb7497ca515c264f46f907a3843709c133dfdbe8
Tiago <talves4@uwo.ca>
2023-01-05 15:33:48 -0500

    added delete DB functionality

--- db-framework.py
+++ db-framework.py
@@ -1,4 +1,5 @@
 import mysql.connector
+import re
 
 mydb = mysql.connector.connect(
   host="localhost",
@@ -12,9 +13,13 @@ print("Welcome to Storify!")
 print("Type 1 if you have an existing database you would like to access")
 print("Type 2 if you would like to create or deleted a database")
 dbChoice = input("Type here: ")
-
+##checking input from user to see if they want to create/delete a database or open an existing one
 if dbChoice == '1':
    print("You choose that you would like to open an existing database please wait...")
+   mycursor.execute("SHOW DATABASES")
+   for y in mycursor:
+      print(y)
+
    dbAlter = input("Type here: ")
 elif dbChoice == '2':
    print("Type 1 to create a database")
@@ -27,7 +32,23 @@ elif dbChoice == '2':
        print("")
        mycursor.execute("SHOW DATABASES")
        for x in mycursor:
-          print(x)
+        filteredx = str(x)
+        ## parses list of database names to remove illegal punction
+        ## this reduces user confusion
+        a = filteredx.replace("'", "").replace("(", "").replace(")", "").replace(",", "")
+        print(a)
+       print("")
+
+       print("Please type the name of the database you would like to delete:")
+       dbDel = input("Type here: ")
+       
+       dropCmd = ("DROP DATABASE {};")
+       mycursor.execute(dropCmd.format(dbDel))
+       print("Change committed")
+       mydb.commit()
+
+
+       
 
 ## mycursor.execute("CREATE DATABASE mydatabase")
 
