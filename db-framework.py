173c3471a9d3eda245dfcea611cf0ddd91cea9e8
Tiago <talves4@uwo.ca>
2023-01-05 15:11:20 -0500

    basic framework for python
    first work on framework

--- db-framework.py
+++ db-framework.py
@@ -1,5 +1,3 @@
-print("hello world")
-
 import mysql.connector
 
 mydb = mysql.connector.connect(
@@ -8,4 +6,29 @@ mydb = mysql.connector.connect(
   password="password"
 )
 
-print(mydb)
+mycursor = mydb.cursor()
+
+print("Welcome to Storify!")
+print("Type 1 if you have an existing database you would like to access")
+print("Type 2 if you would like to create or deleted a database")
+dbChoice = input("Type here: ")
+
+if dbChoice == '1':
+   print("You choose that you would like to open an existing database please wait...")
+   dbAlter = input("Type here: ")
+elif dbChoice == '2':
+   print("Type 1 to create a database")
+   print("Type 2 to delete a database")
+   dbAlter = input("Type here: ")
+   
+   if dbAlter == '1':
+       print("")
+   elif dbAlter == '2':
+       print("")
+       mycursor.execute("SHOW DATABASES")
+       for x in mycursor:
+          print(x)
+
+## mycursor.execute("CREATE DATABASE mydatabase")
+
+
